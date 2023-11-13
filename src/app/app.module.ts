a9c8986fa4b6221163ede400e37caddab1c8b1b7
unknown <dannyrey2018@gmail.com>
2023-11-13 09:53:14 -0600

    added hero and messages services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AddChoreComponent } from './add-chore/add-chore.component';
 import { TitleHeaderComponent } from './title-header/title-header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
@@ -17,7 +19,9 @@ import { HeroesComponent } from './heroes/heroes.component';
     WeekComponent,
     AddChoreComponent,
     TitleHeaderComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'heroes',
@@ -8,9 +8,22 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
- selectedHero?: Hero;
- onSelect(hero:Hero): void{
-  this.selectedHero = hero
- }
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes:Hero[] = [];
+  selectedHero?: Hero;
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
