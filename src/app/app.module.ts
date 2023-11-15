5e7ab161d33c5fb63e2d83b73a9d86ad28b8e997
unknown <dannyrey2018@gmail.com>
2023-11-14 18:20:22 -0600

    new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-import { InMemoryDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
 import { WeekComponent } from './week/week.component';
-import { AddChoreComponent } from './add-chore/add-chore.component';
-import { TitleHeaderComponent } from './title-header/title-header.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     WeekComponent,
-    AddChoreComponent,
-    TitleHeaderComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent,
-    HeroSearchComponent
+    
   ],
   imports: [
     BrowserModule,
@@ -36,13 +20,6 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     BrowserAnimationsModule,
     FormsModule, 
     HttpClientModule,
-    
-  // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-  // and returns simulated server responses.
-  // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, {dataEncapsulation: false}
-    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/week/week.component.spec.ts
+++ src/app/week/week.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { WeekComponent } from './week.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('WeekComponent', () => {
+  let component: WeekComponent;
+  let fixture: ComponentFixture<WeekComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DashboardComponent]
+      declarations: [WeekComponent]
     });
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(WeekComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
